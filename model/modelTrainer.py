419fa435e79426cb11e6ccf5ef91294b2e2e1335
zulja1 <luka.zuljo@gmail.com>
2025-08-21 12:53:30 +0200

    Check lfs budget

--- model/modelTrainer.py
+++ model/modelTrainer.py
@@ -13,12 +13,14 @@ def prediction_model():
     num_of_classes = 25
 
     model = Sequential()
+
     model.add(Conv2D(32, (5, 5), input_shape=(128, 128,1), activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2), padding='same'))
     model.add(Conv2D(64, (5, 5), activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2), padding='same'))
 
     model.add(Flatten())
+    
     model.add(Dense(512, activation='relu'))
     model.add(Dropout(0.5))
     model.add(Dense(128, activation='relu'))
